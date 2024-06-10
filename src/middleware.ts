a62793921836399211506a1e4b02ccbe6e694644
AM0312 <ansh.midha0312@gmail.com>
2024-06-10 15:58:17 +0530

    completed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,15 @@ export { default } from "next-auth/middleware";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
-  if(token && {url.pathname.startsWith('/signin')})
+  if (
+    token &&
+    (url.pathname.startsWith("/signin") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 }
 
 export const config = {
