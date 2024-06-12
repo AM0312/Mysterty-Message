051d93eed7874651be84c208c8e6be477d3bb0f5
AM0312 <ansh.midha0312@gmail.com>
2024-06-12 22:11:37 +0530

    navbar and message card components

---
+++ src/components/messagecard.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { X } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Message } from "@/model/User";
+import { useToast } from "./ui/use-toast";
+import axios from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
+type MessageCardProps = {
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
+
+const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
+  const { toast } = useToast();
+  const handleDeleteConfirm = async () => {
+    const res = await axios.delete<ApiResponse>(
+      `/api/delete-message/${message._id}`
+    );
+    toast({
+      title: res.data.message,
+    });
+    onMessageDelete(message._id as string);
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle></CardTitle>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive">
+              <X className="w-5 h-5" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our database.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeleteConfirm}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </CardHeader>
+      <CardContent />
+    </Card>
+  );
+};
+
+export default MessageCard;
